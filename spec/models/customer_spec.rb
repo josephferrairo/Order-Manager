17951ff238fdb02589cdff851471ca1f550c22f9
josephferrairo <josephferrairo@gmail.com>
2016-06-16 20:59:58 -0400

    added more validations to customer spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Customer, :type => :model do
   it { should belong_to(:user) }
 
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:customercode) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_length_of(:state).is_equal_to(2) }
+  it { should validate_presence_of(:zipcode) }
+  it { should validate_length_of(:zipcode).is_equal_to(5) }
+
 end
