f3f740eb338d463ee7f06986d75d2f064dbcbb15
josephferrairo <josephferrairo@gmail.com>
2016-06-16 20:25:37 -0400

    added controller tests for #show, #index, and added fabricator for customer

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ end
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
   gem 'capybara'
+  gem 'faker'
 end
 
 group :development do
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe CustomersController, type: :controller do
+RSpec.describe CustomersController, :type => :controller do
+  describe "GET #index" do
+    it "returns a successful http request status code" do
+      get :index
 
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "get #show" do
+    it "returns a successfull http request status code" do
+      customer = Fabricate(:customer)
+
+      get :show, id: customer.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
 end
