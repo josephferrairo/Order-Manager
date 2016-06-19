58885c2662d65ebcbe86bf641ed67c99701d41f5
josephferrairo <josephferrairo@gmail.com>
2016-06-18 20:43:37 -0400

    changed root path back to static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    @customers_grid = initialize_grid(Customer,
+    order: 'customers.id',
+    order_direction: 'asc'
+    )
   end
 end
