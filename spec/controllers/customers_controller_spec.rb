ac1c27852386aabe5a83b2f07d3fea05be4bdb8d
josephferrairo <josephferrairo@gmail.com>
2016-06-30 21:33:33 -0400

    added feature and controller tests to work instructions

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, :type => :controller do
+
   describe "GET #index" do
     it "returns a successful http request status code" do
       get :index
